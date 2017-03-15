ac01ff82ba84388bf00fdcaf079c301259bb3cd7
Nicolas Constanty <nicolas.constanty@epitech.eu>
2017-03-15 09:47:16 +0100

    Ajoutez des fichiers projet.

---
+++ CommandBarUWP/CustomControl/Command.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.ObjectModel;
+using System.Windows.Input;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+// The User Control item template is documented at https://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace CommandBarUWP.CustomControl
+{
+    public sealed partial class Command : UserControl
+    {
+        public Command()
+        {
+            this.InitializeComponent();
+        }
+
+        public static readonly DependencyProperty TextProperty =
+        DependencyProperty.Register("Text", typeof(string), typeof(Command), null);
+
+        public string Text
+        {
+            get { return (string)GetValue(TextProperty); }
+            set { SetValue(TextProperty, value); }
+        }
+
+        public static readonly DependencyProperty SubCommandsProperty =
+        DependencyProperty.Register("SubCommands", typeof(ObservableCollection<string>), typeof(Command), new PropertyMetadata(new ObservableCollection<string>()));
+
+        public ObservableCollection<string> SubCommands
+        {
+            get { return (ObservableCollection<string>)GetValue(SubCommandsProperty); }
+            set { SetValue(SubCommandsProperty, value); }
+        }
+
+        public static readonly DependencyProperty ActionProperty
+      = DependencyProperty.Register("Action", typeof(ICommand), typeof(Command), null);
+
+        public ICommand Action
+        {
+            get { return (ICommand)GetValue(ActionProperty); }
+            set { SetValue(ActionProperty, value); }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Action.Execute(this);
+        }
+    }
+}
---
+++ CommandBarUWP/CustomControl/MenuBar.xaml.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Windows.Input;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+// The User Control item template is documented at https://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace CommandBarUWP.CustomControl
+{
+    public sealed partial class MenuBar : UserControl
+    {
+        private bool _expend = false;
+        private Command _current = null;
+
+        public MenuBar()
+        {
+            this.InitializeComponent();
+        }
+
+        public static readonly DependencyProperty MainCommandsProperty =
+        DependencyProperty.Register("MainCommands", typeof(ObservableCollection<FrameworkElement>), typeof(MenuBar), new PropertyMetadata(new ObservableCollection<FrameworkElement>()));
+
+        public ObservableCollection<FrameworkElement> MainCommands
+        {
+            get { return (ObservableCollection<FrameworkElement>)GetValue(MainCommandsProperty); }
+            set {
+                SetValue(MainCommandsProperty, value);
+            }
+        }
+
+        public static readonly DependencyProperty CommanHeightProperty =
+        DependencyProperty.Register("CommandHeight", typeof(int), typeof(MenuBar), null);
+
+        public int CommandHeight
+        {
+            get { return (int)GetValue(CommanHeightProperty); }
+            set { SetValue(CommanHeightProperty, value); }
+        }
+
+        private void Expend()
+        {
+            SubMenuPanel.Children.Clear();
+            foreach (var cmd in _current.SubCommands)
+            {
+                Button btn = new Button();
+                btn.Content = cmd;
+                SubMenuPanel.Children.Add(btn);
+            }
+            SubMenuPanel.Visibility = Visibility.Visible;
+            _expend = true;
+        }
+
+        private void Collapse()
+        {
+            SubMenuPanel.Visibility = Visibility.Collapsed;
+        }
+
+        class CommandHandler : ICommand
+        {
+            public event EventHandler CanExecuteChanged;
+
+            private Action _action;
+
+            public CommandHandler(Action action)
+            {
+                this._action = action;
+            }
+
+            public bool CanExecute(object parameter)
+            {
+                return true;
+            }
+
+            public void Execute(object parameter)
+            {
+                this._action();
+            }
+        }
+
+        public ICommand SelectMenuCommand
+        {
+            get
+            {
+                return new CommandHandler(() => this.SelectMenu());
+            }
+        }
+
+        public void SelectMenu(Command cmd)
+        {
+            if (cmd == _current || cmd == null || _expend == true)
+                return;
+            _current = cmd;
+           Expend();
+        }
+    }
+}
